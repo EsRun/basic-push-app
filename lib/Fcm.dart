6137972c2d55b63aef1d76568adce5d71a2843ef
비엔나 <sure@paran.com>
2024-01-02 15:52:58 +0900

    add : fcm init

---
+++ lib/Fcm.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class Fcm{
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  Future<void> initNoti() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+    final fcmToken = await messaging.getToken();
+    print(fcmToken);
+    print('User granted permission: ${settings.authorizationStatus}');
+    initPush();
+  }
+
+  Future<void> initPush() async{
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,18 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'Fcm.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  Fcm().initNoti();
+
   runApp(const MyApp());
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  final fcmToken = await FirebaseMessaging.instance.getToken();
-  print(fcmToken);
 
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
 
-  print('User granted permission: ${settings.authorizationStatus}');
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    print('Got a message whilst in the foreground!');
-    print('Message data: ${message.data}');
-
-    if (message.notification != null) {
-      print('Message also contained a notification: ${message.notification}');
-    }
-  });
 }
 
 class MyApp extends StatelessWidget {
